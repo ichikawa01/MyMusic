3b0bc2edbbaa6f47b77c6074e7dd7d6d56966cea
IchikawaRyo <ryo.i.343826@gmail.com>
2025-05-04 19:12:12 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by 市川涼 on 2025/05/04.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
